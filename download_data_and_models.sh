58ba69259dd1bd3391bf9c8b0fe93912df0125b1
andrekassis <45173083+andrekassis@users.noreply.github.com>
2025-12-29 14:09:11 -0500

    Update download_data_and_models.sh

--- download_data_and_models.sh
+++ download_data_and_models.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-curl -o datasets.tar.gz https://vault.cs.uwaterloo.ca/s/tzckM6g6PYo9Prn/download
-curl -o pretrained_models.tar.gz.aa https://vault.cs.uwaterloo.ca/s/ecQd9W24oDwwKja/download
-curl -o pretrained_models.tar.gz.ab https://vault.cs.uwaterloo.ca/s/mL6ZxC9itgc9wCR/download
-curl -o pretrained_models.tar.gz.ac https://vault.cs.uwaterloo.ca/s/iffQ9GPSSqyx2Zt/download
-curl -o pretrained_models.tar.gz.ad https://vault.cs.uwaterloo.ca/s/9kqjC7KAPQkK5DX/download
-curl -o pretrained_models.tar.gz.ae https://vault.cs.uwaterloo.ca/s/iJ8aQHcLeojACR5/download
-curl -o pretrained_models.tar.gz.af https://vault.cs.uwaterloo.ca/s/LqKMxiMXC5TkwQx/download
-curl -o pretrained_models.tar.gz.ag https://vault.cs.uwaterloo.ca/s/9T5ZzYa26HqTAYB/download
+curl -L -o datasets.tar.gz https://nextcloud.cs.uwaterloo.ca/s/iZRD2SdgjKZRskR/download
+curl -L -o pretrained_models.tar.gz.aa https://nextcloud.cs.uwaterloo.ca/s/tCdKk8x8z2na4mA/download
+curl -L -o pretrained_models.tar.gz.ab https://nextcloud.cs.uwaterloo.ca/s/7XMBndAdPBSwBNE/download
+curl -L -o pretrained_models.tar.gz.ac https://nextcloud.cs.uwaterloo.ca/s/kpNFGddk2tbK2Hn/download
+curl -L -o pretrained_models.tar.gz.ad https://nextcloud.cs.uwaterloo.ca/s/FEnfHBQctWMLYKE/download
+curl -L -o pretrained_models.tar.gz.ae https://nextcloud.cs.uwaterloo.ca/s/swW6PT4kXx3W3HN/download
+curl -L -o pretrained_models.tar.gz.af https://nextcloud.cs.uwaterloo.ca/s/8Q9yPd94kRKfXgf/download
+curl -L -o pretrained_models.tar.gz.ag https://nextcloud.cs.uwaterloo.ca/s/jZjNMC4wdemccEf/download
 
 tar -zxvf datasets.tar.gz
 cat pretrained_models.tar.gz.* | tar xzvf -
